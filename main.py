9131b33abdb8b9f117144afc9e24a58f0d9f28ce
TakashiHirokane <hirokanetakashi@gmail.com>
2021-05-09 03:28:07 +0900

    add Templates

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-def dashborad():
-    return {"dashborad": "Home Page"}
+def dashborad(request: Request):
+    """
+    ダッシュボードを表示する
+    """
+    return templates.TemplateResponse("home.html", {
+        "request": request
+    })
+
+@app.post("/stock")
+def create_stock():
+    """
+    stockを作成し、DB登録する
+    """
+    return {
+        "code": "success",
+        "message": "stock created"
+    }
