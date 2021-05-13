f34d3c214281ada1157d9d1285c932fd2799fe0e
TakashiHirokane <hirokanetakashi@gmail.com>
2021-05-13 22:27:29 +0900

    connecting frontend to api

--- main.py
+++ main.py
@@ -24,12 +24,37 @@ def get_db():
         db.close()
 
 @app.get("/")
-def dashborad(request: Request):
+def home(request: Request, forward_pe = None, dividend_yield = None, ma50 = None, ma200 = None, db: Session = Depends(get_db)):
     """
-    ダッシュボードを表示する
+    show all stocks in the database and button to add more
+    button next to each stock to delete from database
+    filters to filter this list of stocks
+    button next to each to add a note or save for later
     """
+
+    stocks = db.query(Stock)
+
+    if forward_pe:
+        stocks = stocks.filter(Stock.forward_pe < forward_pe)
+
+    if dividend_yield:
+        stocks = stocks.filter(Stock.dividend_yield > dividend_yield)
+    
+    if ma50:
+        stocks = stocks.filter(Stock.price > Stock.ma50)
+    
+    if ma200:
+        stocks = stocks.filter(Stock.price > Stock.ma200)
+    
+    stocks = stocks.all()
+
     return templates.TemplateResponse("home.html", {
-        "request": request
+        "request": request, 
+        "stocks": stocks, 
+        "dividend_yield": dividend_yield,
+        "forward_pe": forward_pe,
+        "ma200": ma200,
+        "ma50": ma50
     })
 
 def fetch_stock_data(id: int):
